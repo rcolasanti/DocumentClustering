454dec3a3b9504ea4fc440a1cea133f248785ee7
Dr Ric Colasanti <ric.colasanti@gmail.com>
2015-04-04 15:50:53 +0000

    td-idf added to the program

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # DocumentClustering
 Document clustering with stand alone Python . This is reworking of "Document clustering with Python" by  http://brandonrose.org/clustering. This version will run in a Tkinter window.  
+To use nltk you have to downlad any corpus to a directory from nltk:
+python -m nltk.downloader
+Then link it to you code nltk.data.path.append('./nltk_data/') 
+if using smoowball stemmer have to downlad the data for that from the above. In the models pick snowball_datals
+
+"Document clustering with Python" uses sklearn fro analysis in this project I will be "rollong my own" Start with calculating TFIDF
+
+Taken from:
+http://www.tfidf.com/
+
+
+Typically, the tf-idf weight is composed by two terms: the first computes the normalized Term Frequency (TF), aka. the number of times a word appears in a document, divided by the total number of words in that document; the second term is the Inverse Document Frequency (IDF), computed as the logarithm of the number of the documents in the corpus divided by the number of documents where the specific term appears.
+
+TF: Term Frequency, which measures how frequently a term occurs in a document. Since every document is different in length, it is possible that a term would appear much more times in long documents than shorter ones. Thus, the term frequency is often divided by the document length (aka. the total number of terms in the document) as a way of normalization: 
+
+TF(t) = (Number of times term t appears in a document) / (Total number of terms in the document).
+
+IDF: Inverse Document Frequency, which measures how important a term is. While computing TF, all terms are considered equally important. However it is known that certain terms, such as "is", "of", and "that", may appear a lot of times but have little importance. Thus we need to weigh down the frequent terms while scale up the rare ones, by computing the following: 
+
+IDF(t) = log_e(Total number of documents / Number of documents with term t in it).
+
+See below for a simple example.
+
+Example:
+
+Consider a document containing 100 words wherein the word cat appears 3 times. The term frequency (i.e., tf) for cat is then (3 / 100) = 0.03. Now, assume we have 10 million documents and the word cat appears in one thousand of these. Then, the inverse document frequency (i.e., idf) is calculated as log(10,000,000 / 1,000) = 4. Thus, the Tf-idf weight is the product of these quantities: 0.03 * 4 = 0.12.
+
+
+
+This is then used to look at differances between the documents
+method used is cosine similarity informationn fom here:
+https://janav.wordpress.com/2013/10/27/tf-idf-and-cosine-similarity/
--- document_clustering.py
+++ document_clustering.py
@@ -20,45 +20,85 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 #  
-#  
+#  /home/pi/Programming/Projects/DocumentClustering/Data/nltk_data/
 
 import string
+import nltk
+import re
+import math
+from nltk.stem.snowball import SnowballStemmer
 
 class DocumentClustering:
     def __init__(self):
         self.titles=[]
-        self.ascii_codes= string.ascii_letters + string.punctuation + string.digits+' '
+        self.synopsis_frequency=[]
+        self.corpus_frequency={}
+        self.ascii_codes= string.ascii_letters + ' '#string.punctuation + string.digits+' '
+        self.data_path = "/home/pi/Programming/Projects/DocumentClustering/Data/"
+        nltk.data.path.append(self.data_path+"nltk_data/")
+        self.stemmer = SnowballStemmer("english")
+        self.stopwords = nltk.corpus.stopwords.words('english')
+        #for word in self.stopwords:
+        #    print word,
         
     def read_titles(self):
-        t_file = "/home/pi/Programming/Projects/DocumentClustering/Data/title_list.txt"
+        t_file =self.data_path+"text/title_list.txt"
         rf = open(t_file, 'r')
         for line in rf:
             newline = line.rstrip('\r\n')
             print newline
             self.titles.append(newline)
             
-    def read_synopses(self):
-        t_file = "/home/pi/Programming/Projects/DocumentClustering/Data/synopses_list_imdb.txt"
-        synopses=''
-        count = 0
+    def read_synopsis(self):
+        t_file =self.data_path+"text/synopsis_list_imdb.txt"
+        synopsis=''
         rf = open(t_file, 'r')
+        count = 0
         for line in rf:
             if 'BREAKS HERE' in line:
-                print count,synopses
-                synopses=''
+                word_frequency = self.parse_synopsis(synopsis)
+                self.synopsis_frequency.append(word_frequency)
+                synopsis=''
+                print count
                 count+=1
             else:
                 for c in line:
                     if c in self.ascii_codes:
-                        synopses+=c
+                        synopsis+=c
+        print
+        for key in  self.corpus_frequency:
+            self.corpus_frequency[key]= math.log(count/self.corpus_frequency[key])
+            print key,self.corpus_frequency[key]
+                        
+    def parse_synopsis(self,synopsis):
+        document_frequency={}
+        for sent in nltk.sent_tokenize(synopsis):
+            for word in nltk.word_tokenize(sent):
+                l_word = word.lower()
+                if not l_word in self.stopwords:
+                    s_word = self.stemmer.stem(l_word)
+                    
+                    if s_word in document_frequency:
+                        document_frequency[s_word]+=1
+                    else:
+                        document_frequency[s_word]=1
         
+        number_of_words = len(document_frequency)
+        for key in document_frequency:
+            document_frequency[key] = document_frequency[key]/number_of_words 
+            if key in self.corpus_frequency:
+                self.corpus_frequency[key]+=1
+            else:
+                self.corpus_frequency[key]=1
 
+        return  document_frequency
 
 
 def main():
     dc = DocumentClustering()
     #dc.read_titles()
-    dc.read_synopses()
+    dc.read_synopsis()
+    print len(dc.corpus_frequency)
     return 0
 
 if __name__ == '__main__':
